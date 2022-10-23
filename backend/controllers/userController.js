3591c00d652fed615ddc9cd32ae61dcc6d13cb1c
Tim Quattrochi <timq82@gmail.com>
2022-10-23 15:35:11 -0400

    established mongo db connection

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,21 @@
-const registerUser = (req, res) => {
-    res.send('Register Route')
-}
+const asyncHandler = require('express-async-handler')
 
-const loginUser = (req, res) => {
+
+//http://localhost:3001/api/users for postman testing
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('please enter all fields')
+    }
+    res.send('Register Route')
+})
+
+//http://localhost:3001/api/users/login for postman testing
+const loginUser = asyncHandler(async (req, res) => {
     res.send('login Route')
-}
+})
 
 module.exports = {
     registerUser,
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,24 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 5000
 
+connectDB()
+
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.status(200).send('api endpoint')
 })
 
 
-
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`))
