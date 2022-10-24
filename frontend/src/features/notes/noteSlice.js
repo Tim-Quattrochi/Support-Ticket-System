d74f6ff7e5164bf1d038c4bf9197a4bf379afd45
Tim Quattrochi <timq82@gmail.com>
2022-10-24 18:50:35 -0400

    add note functionality is working

--- frontend/src/features/notes/noteSlice.js
+++ frontend/src/features/notes/noteSlice.js
@@ -30,11 +30,11 @@ export const getNotes = createAsyncThunk(
 
 //create ticket note
 export const createNote = createAsyncThunk(
-    'notes/getAll',
-    async (ticketId, thunkAPI) => {
+    'notes/create',
+    async ({ noteText, ticketId }, thunkAPI) => {
         try {
             const token = thunkAPI.getState().auth.user.token
-            return await noteService.getNotes(ticketId, token)
+            return await noteService.createNote(noteText, ticketId, token)
         } catch (error) {
             const message = (error.response &&
                 error.response.data &&
@@ -68,6 +68,11 @@ export const noteSlice = createSlice({
                 state.isError = true
                 state.message = action.payload
             })
+            .addCase(createNote.fulfilled, (state, action) => {
+                state.notes.push(action.payload)
+            })
+
+
 
     }
 })
