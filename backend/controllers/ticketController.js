e05f4c597b9b16b2b267123d1a3478910b7f76c7
Tim Quattrochi <timq82@gmail.com>
2022-10-23 23:54:39 -0400

    added find ticket by id authorization using the JWT as id

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -18,6 +18,31 @@ const getTickets = asyncHandler(async (req, res) => {
     res.status(200).json(tickets)
 })
 
+//GET /api/tickets/:id
+const getTicket = asyncHandler(async (req, res) => {
+    //Get user using the id in the JWT
+    const user = await User.findById(req.user.id)
+
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const ticket = await Ticket.findById(req.params.id)
+
+    if (!ticket) {
+        res.status(404)
+        throw new Error('Ticket not found')
+    }
+
+    if (ticket.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('Not authorized.')
+    }
+
+    res.status(200).json(ticket)
+})
+
 //POST create tickets, /api/tickets
 const createTicket = asyncHandler(async (req, res) => {
     const { product, description } = req.body
@@ -46,5 +71,7 @@ const createTicket = asyncHandler(async (req, res) => {
 
 module.exports = {
     getTickets,
-    createTicket
+    getTicket,
+    createTicket,
+
 }
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { getTickets, createTicket } = require('../controllers/ticketController')
+const { getTicket, getTickets, createTicket } = require('../controllers/ticketController')
 
 
 const { protect } = require('../middleware/authMiddleware')
 
 router.route('/').get(protect, getTickets).post(protect, createTicket)
 
-module.exports = router
+router.route('/:id').get(protect, getTicket)
+
+module.exports = router 
