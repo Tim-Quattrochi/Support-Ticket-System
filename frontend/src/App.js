770437d15fc03325f71f1dd7da46819420e7de6a
Tim Quattrochi <timq82@gmail.com>
2022-10-23 21:07:56 -0400

    added sign up functionality, persist with local storage, implemented redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+import Header from './components/Header';
+import Home from './pages/Home';
+import LogIn from './pages/LogIn';
+import Register from './pages/Register';
+
+
 
 function App() {
   return (
-    <div>
-      My app
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<LogIn />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 
