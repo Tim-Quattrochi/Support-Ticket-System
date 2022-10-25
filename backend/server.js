9f2a8d4c8c6413328ddd6b6774adbad373d12526
Tim Quattrochi <timq82@gmail.com>
2022-10-25 17:57:14 -0400

    removed app.get slash

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,6 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-    res.status(200).send('api endpoint')
-})
-
 
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
